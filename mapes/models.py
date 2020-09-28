157e4faf68e6d77694e366c8960b4cca7b7656d5
antonionoronha <antonio.noronhaneto@gmail.com>
2020-09-27 21:27:49 -0300

    final commit

--- mapes/models.py
+++ mapes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Consulta(models.Model):
+    numero_guia = models.IntegerField()
+    cod_medico = models.IntegerField()
+    nome_medico = models.TextField(max_length=100)
+    data_consulta = models.DateField()
+    valor_consulta = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Exame(models.Model):
+    numero_guia_consulta = models.ForeignKey(Consulta, on_delete=models.CASCADE)
+    exame = models.TextField(max_length=100)
+    valor_exame = models.DecimalField(max_digits=5,decimal_places=2)
+
--- mapes/views.py
+++ mapes/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from django.db.models import Count, Sum
+from django.db import models
+from .models import Consulta
+from .models import Exame
 
-# Create your views here.
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+def consulta_list(request):
+
+    medicos = Consulta.objects.all()
+    qs = Consulta.objects.all()
+    nome_medico = request.GET.get('nome_medico')
+    data_inicio = request.GET.get('data_inicio')
+    data_fim    = request.GET.get('data_fim')
+
+    if is_valid_queryparam(nome_medico):
+        qs = qs.filter(nome_medico__iexact=nome_medico)
+        #print(qs[0])
+        qs = qs.annotate(num_exame=Count('exame'))
+        qs = qs.annotate(total=Sum('exame__valor_exame'))
+        #print(qs[1].total)
+        for q in qs:
+            print(q.numero_guia,q.num_exame,q.total)
+
+    if is_valid_queryparam(data_inicio) and is_valid_queryparam(data_fim):
+        qs = qs.filter(data_consulta__range=['2020-09-27','2019-01-01'])
+        print(qs)
+        #qs = qs.filter(data_consulta__range=[data_inicio,data_fim])
+    elif is_valid_queryparam(data_inicio):
+        qs = qs.filter(data_consulta__year__gte=2019)
+    elif is_valid_queryparam(data_fim):
+        qs = qs.filter(data_consulta__lt='2020-09-27')
+        print(qs)
+        print(data_fim)
+
+    context = {
+        'consultas': qs,
+        'medicos': medicos,
+    }
+
+    return render(request,"list.html", context)
